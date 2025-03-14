4165d64807809bb144b331715d6c5ed7270c358f
Mikhael <butylkin990088@mail.ru>
2025-03-14 17:49:27 +0300

    add .env

--- src/api/endpoint.js
+++ src/api/endpoint.js
@@ -1,6 +1,6 @@
-export const base_url = 'http://127.0.0.1:8000';
-// В будущем
-// export const base_url = `${process.env.BASE_URL_SERVER_API}`;
+// export const base_url = 'http://127.0.0.1:8000';
+const apiUrl = process.env.REACT_APP_API_URL;
+export const base_url = `${apiUrl}`;
 
 
 export const endpoint = {
